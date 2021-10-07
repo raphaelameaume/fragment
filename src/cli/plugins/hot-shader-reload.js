d55aeaf5d7d22b2345753c32d1278b3ce3d55b68
Raphaël Améaume <raphael.ameaume@gmail.com>
2021-10-07 13:12:13 +0200

    Add filepath to shader files

--- src/cli/plugins/hot-shader-reload.js
+++ src/cli/plugins/hot-shader-reload.js
@@ -1,14 +1,42 @@
 import path from "path";
 import glslify from "glslify";
 
-export default function hotShaderReload() {
+export default function hotShaderReload({ wss }) {
     const fileRegex = /\.(?:frag|vert|glsl|vs|fs)$/
     
     return {
         name: 'hot-shader-reload',
+        handleHotUpdate: async ({ file, read }) => {
+            if (fileRegex.test(file)) {
+                let src = await read();
+                let source = glslify(src);
+
+                wss.send({
+                    type: 'custom',
+                    event: 'shader-update',
+                    data: {
+                        filepath: file,
+                        source
+                    },
+                });
+
+                return [];
+            }
+        },
         transform: (src, id) => {
             if (fileRegex.test(id)) {
                 let source = glslify(src);
+
+                let keyword = `void`;
+                let shaderParts = source.split(keyword);
+                let hint = `// <filepath://${id}>`
+
+                source = `
+${shaderParts[0]}
+${hint}
+${keyword}${shaderParts[1]}
+                `;
+
                 return {
                     code: `export default ${JSON.stringify(source)}`,
                     map: null // provide source map if available
