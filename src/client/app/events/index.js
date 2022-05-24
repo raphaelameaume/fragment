2676dfc256913acbfc9c17273662e6a46d9bbfb3
Raphaël Améaume <raphael.ameaume@gmail.com>
2022-05-24 13:53:10 +0200

    Remove unused events

--- src/client/app/events/index.js
+++ src/client/app/events/index.js
@@ -9,7 +9,3 @@ export { emit };
 export default eventBus;
 
 export const TRANSITION_CHANGE = '@fragment/TRANSITION_CHANGE';
-export const PREVIEW_MOUNT = '@fragment/PREVIEW_MOUNT';
-export const PREVIEW_UPDATE = '@fragment/PREVIEW_UPDATE';
-export const PREVIEW_AFTER_UPDATE = '@fragment/PREVIEW_AFTER_UPDATE';
-export const PREVIEW_BEFORE_UPDATE = '@fragment/PREVIEW_BEFORE_UPDATE';
