2f1e7ed541de7cbd29d13d08abe96cafab0eb3bc
Raphaël Améaume <raphael.ameaume@gmail.com>
2022-06-02 17:40:29 +0200

    Support vec3 and vec4 inputs

--- src/client/app/ui/fields/ColorInput.svelte
+++ src/client/app/ui/fields/ColorInput.svelte
@@ -1,6 +1,6 @@
 <script>
 import { createEventDispatcher } from "svelte";
-import { colord } from "colord"; 
+import { colorToHex, colorToRGBA, colorToRGBAString, colorToRGBString, colorToVec3String, colorToVec4String, componentsToVec4String, FORMATS, getColorFormat, hexToRGBA, hexToVec3String, hexToVec4String, isColor,  isRGBAString,  isVec4String,  RGBAToRGBAString, stringToRGBA } from "../../utils/color.utils.js";
 import TextInput from "./TextInput.svelte";
 import Field from "../Field.svelte";
 
@@ -9,43 +9,91 @@ export let name;
 
 const dispatch = createEventDispatcher();
 
-let color = colord(value);
-let hasAlpha = false;
-let alpha = 0;
+let hexValue = colorToHex(value);
+let isInputDriven = true;
 
-if (value.includes("rgba")) {
-    hasAlpha = true;
-    alpha = color.alpha();
+let override = false;
+
+let format = getColorFormat(value);
+let textValue = formatColorFromHex(hexValue);
+let alpha = 1;
+
+$: hasAlpha = typeof textValue === "string" && (isRGBAString(textValue) || isVec4String(textValue));
+$: {
+    if (isInputDriven) {
+        textValue = formatColorFromHex(hexValue);
+    } else {
+        hexValue = colorToHex(textValue);
+    }
 }
 
-$: textValue = hasAlpha ? colord(`rgb(${color.rgba.r}, ${color.rgba.g}, ${color.rgba.b})`).toHex() : color.toHex();
-$: inputValue = colord(textValue).toHex();
+$: {
+    if (hasAlpha) {
+        const [r, g, b, a = 1] = colorToRGBA(textValue);
+        alpha = a;
+    }
+}
 
-function handleChangeColor(event) {
-    textValue = event.currentTarget.value;
-    inputValue = event.currentTarget.value;
+function handleBlur() {
+    dispatch('change', textValue);
+}
 
-    dispatch('change', event.currentTarget.value);
+function formatColorFromHex(hex) {
+    if (override) return textValue;
+
+    if (format === FORMATS.HEX_STRING) return hex;
+    if (format === FORMATS.VEC3_STRING) return hexToVec3String(hex);
+    if (format === FORMATS.RGB_STRING) return hexToRGBString(hex);
+
+    let components = hexToRGBA(hex);
+
+    if (hasAlpha && alpha !== 1) {
+        components[3] = alpha;
+    }
+
+    if (format === FORMATS.RGBA_STRING) return RGBToRGBAString(components);
+    if (format === FORMATS.VEC4_STRING) return componentsToVec4String(components);
 }
 
 function onChangeText(event) {
-    textValue = event.currentTarget.value;
+    isInputDriven = false;
 
-    dispatch('change', event.currentTarget.value);
+    const { value } = event.currentTarget;
+
+    if (isColor(value)) {
+        format = getColorFormat(value);
+        textValue = value;
+
+        dispatch('change', value);
+    }
 }
+
+function onChangeAlpha(event) {
+    if (format === FORMATS.RGBA_STRING) {
+        const [r, g, b] = hexToRGBA(textValue);
+
+        textValue = RGBAToRGBAString([r, g, b, event.detail]);
+    } else if (format === FORMATS.VEC4_STRING) {
+        const [r, g, b] = hexToRGBA(hexValue);
+
+        textValue = componentsToVec4String([r, g, b, event.detail]);
+    }
+}
+
+
 
 </script>
 
 <div class="color-input">
     <div class="layout">
-        <div class="mirror" style="--currentColor: {textValue}">
+        <div class="mirror" style="--currentColor: {hexValue}; --opacity: {alpha}">
             <!-- svelte-ignore -->
-            <input class="input" type="color" value={inputValue} {name} on:input={handleChangeColor} />
+            <input class="input" type="color" bind:value={hexValue} {name} on:blur={handleBlur} on:input={() => isInputDriven = true} />
         </div>
         <TextInput value={textValue} on:input={onChangeText} {name} on:change={onChangeText} />
     </div>
     {#if hasAlpha }
-        <Field key="alpha" value={alpha} params={{min: 0, max: 1, step: 0.01}}></Field>
+        <Field key="alpha" value={alpha} params={{min: 0, max: 1, step: 0.01}} on:change={onChangeAlpha}></Field>
     {/if }
 </div>
 
@@ -67,7 +115,7 @@ function onChangeText(event) {
     
     height: var(--height-input);
 
-    border-radius: var(--border-radius-inputut);
+    border-radius: var(--border-radius-input);
     box-shadow: inset 0 0 0 1px var(--color-border-input);
 }
 
@@ -82,11 +130,16 @@ function onChangeText(event) {
     bottom: var(--gap);
 
     background-color: var(--currentColor);
-    border-radius: calc(var(--border-radius-inputut) * 0.5);
+    border-radius: calc(var(--border-radius-input) * 0.5);
+    opacity: var(--opacity, 1);
 }
 
 .mirror:hover {
-    box-shadow: inset 0 0 0 1px var(--color-active);
+    box-shadow: inset 0 0 0 1px var(--box-shadow-color, var(--color-active));
+}
+
+.mirror:focus-within {
+    box-shadow: 0 0 0 2px var(--box-shadow-color, var(--color-active));
 }
 
 .input {
--- src/client/app/utils/color.utils.js
+++ src/client/app/utils/color.utils.js
@@ -24,17 +24,35 @@ export function nameToRGBA(colorName, element = "temp") {
  * @return {array}
  */
 export function stringToRGBA(color) {
-	const match = color.match(/rgba?\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3})\)?(?:, ?(\d(?:\.\d?))\))?/);
+	const match = color.match(/rgba?\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3})\)?(?:, ?(\d(?:\.\d+?))\))?/);
 
 	if (match) {
 		return [
 			parseInt(match[1]),
 			parseInt(match[2]),
 			parseInt(match[3]),
-			match[4] ? parseInt(match[4]) : 1];
+			match[4] ? Number(match[4]) : 1];
 	}
 
-	return [null, null, null, null];
+	return [];
+}
+
+export function vecToRGBA(color) {
+	const match = color.match(/vec[3-4]?\((\d*(?:\.\d*?)), ?(\d*(?:\.\d*?)), ?(\d*(?:\.\d*))?(?:, ?(\d(?:\.\d*?))\))?/);
+
+	if (match) {
+		return [
+			Math.min(255, Math.max(Math.round(Number(match[1]) * 255), 0)),
+			Math.min(255, Math.max(Math.round(Number(match[2]) * 255), 0)),
+			Math.min(255, Math.max(Math.round(Number(match[3]) * 255), 0)),
+			match[4] ? Math.min(1, Math.max(Number(match[4]), 0)) : 1];
+	}
+
+	return [];
+}
+
+export function vecToHex(color) {
+	return RGBAToHex(vecToRGBA(color));
 }
 
 export function hexToRGBA(color) {
@@ -60,43 +78,144 @@ export function hexToRGBA(color) {
 	];
 }
 
+/**
+ * Convert a hexadecimal string to 
+ * @param {string} color 
+ * @returns 
+ */
+export function hexToVec3String(color) {
+	return componentsToVec3String(hexToRGBA(color));
+}
+
+export function hexToVec4String(color) {
+	return componentsToVec4String(hexToRGBA(color));
+}
+
 export function colorToRGBA(color) {
-	if (color[0] === "#") return hexToRGBA(color);
+	if (isHexString(color)) return hexToRGBA(color);
+	if (isRGBAString(color)) return stringToRGBA(color);
+	if (isRGBString(color)) return stringToRGBA(color);
+	if (isVec3String(color)) return vecToRGBA(color);
+	if (isVec4String(color)) return vecToRGBA(color);
+	
 	if (!color.includes('rgb')) return nameToRGBA(color);
-	if (color.includes('rgb')) return stringToRGBA(color);
+
+	console.error(`colorToRGBA :: cannot parse color.`);
+}
+
+export function colorToVec3String(color) {
+	return componentsToVec3String(colorToRGBA(color));
+}
+
+export function colorToVec4String(color) {
+	return componentsToVec4String(colorToRGBA(color));
+}
+
+export function componentsToVec3String(components = []) {
+	const [ r = 0, g = 0, b = 0] = components;
+
+	let rn = `${Math.round(r / 255 * 100000) / 100000}`;
+	let gn = `${Math.round(g / 255 * 100000) / 100000}`;
+	let bn = `${Math.round(b / 255 * 100000) / 100000}`;
+
+	if (r === 255 || r === 0) {
+		rn = `${rn}.0`;
+	}
+
+	if (g === 255 || g === 0) {
+		gn = `${gn}.0`;
+	}
+
+	if (b === 255 || b === 0) {
+		bn = `${bn}.0`;
+	}
+
+	return `vec3(${rn}, ${gn}, ${bn})`;
+}
+
+export function componentsToVec4String(components = []) {
+	const [ r = 0, g = 0, b = 0, a = 1] = components;
+
+	let rn = `${Math.round(r / 255 * 100000) / 100000}`;
+	let gn = `${Math.round(g / 255 * 100000) / 100000}`;
+	let bn = `${Math.round(b / 255 * 100000) / 100000}`;
+	let an = `${a}`;
+
+	if (r === 255 || r === 0) {
+		rn = `${rn}.0`;
+	}
+
+	if (g === 255 || g === 0) {
+		gn = `${gn}.0`;
+	}
+
+	if (b === 255 || b === 0) {
+		bn = `${bn}.0`;
+	}
+
+	if (a === 1 || a === 0) {
+		an = `${an}.0`;
+	}
+
+	return `vec4(${rn}, ${gn}, ${bn}, ${an})`;
 }
 
 export function colorToHex(color) {
-	if (color[0] === "#") return color;
+	if (isHexString(color)) return color;
+	if (isRGBString(color)) return stringToHex(color);
+	if (isRGBAString(color)) return stringToHex(color);
+	if (isVec3String(color)) return vecToHex(color);
+	if (isVec4String(color)) return vecToHex(color);
 	if (!color.includes('rgb')) return nameToHex(color);
-	if (color.includes('rgb')) return stringToHex(color);
 }
 
 export function colorToRGBString(color) {
 	const [r, g, b] = colorToRGBA(color);
 
-	return `rgb(${r}, ${g}, ${b})`;
+	return RGBtoRGBString([r, g, b]);
+}
+
+export function hexToRGBString(color) {
+	return RGBToRGBString(hexToRGBA(color));
+}
+
+export function hexToRGBAString(color) {
+	return RGBAToRGBString(hexToRGBA(color));
 }
 
 export function colorToRGBAString(color) {
-	const [r, g, b, a] = colorToRGBA(color);
+	return RGBAToRGBAString(colorToRGBA(color));
+}
+
+export function RGBtoRGBString(components) {
+	const [ r, g, b ] = components;
+
+	return `rgb(${r}, ${g}, ${b})`;
+}
+
+export function RGBAToRGBAString(components = []) {
+	const [ r = 0, g = 0, b = 0, a = 1 ] = components;
 
 	return `rgba(${r}, ${g}, ${b}, ${a})`;
 }
 
+export function RGBAToHex(components) {
+	return RGBToHex(components);
+}
+
 export function stringToHex(color) {
-	return rgbToHex(stringToRGBA(color));
+	return RGBToHex(stringToRGBA(color));
 }
 
 export function nameToHex(color) {
-	return rgbToHex(nameToRGBA(color));
+	return RGBToHex(nameToRGBA(color));
 }
 
 export function componentToHex(c) {
 	return c.toString(16).padStart(2, 0);
 }
 
-export function rgbToHex(components = [0, 0, 0]) {
+export function RGBToHex(components = [0, 0, 0]) {
 	const [r, g, b] = components;
 
 	return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
@@ -114,6 +233,14 @@ export function isRGBString(color, isString = typeof color === "string") {
 	return isString && color.includes("rgb");
 }
 
+export function isVec3String(color, isString = typeof color === "string") {
+	return isString && color.includes("vec3(");
+}
+
+export function isVec4String(color, isString = typeof color === "string") {
+	return isString && color.includes("vec4(");
+}
+
 export function isRGBAArray(value) {
 	return Array.isArray(value) && value.length === 4 && value.every((c, i) => i < 3 ? (c <= 255) : (c <= 1));
 }
@@ -129,10 +256,12 @@ export function isName(value) {
 }
 
 export function isColor(value) {
-	let isString = typeof color === "string";
+	let isString = typeof value === "string";
 
 	if (isHexString(value, isString)) return true;
 	if (isRGBString(value, isString)) return true;
+	if (isVec3String(value, isString)) return true;
+	if (isVec4String(value, isString)) return true;
 	if (isRGBArray(value)) return true;
 	if (isRGBAArray(value)) return true;
 	if (isName(value)) return true;
@@ -146,6 +275,8 @@ export const FORMATS = {
 	RGBA_STRING: "rgba-string",
 	RGB_ARRAY: "rgb-array",
 	RGBA_ARRAY: "rgba-array",
+	VEC3_STRING: "vec3-string",
+	VEC4_STRING: "vec4-string",
 };
 
 export function getColorFormat(value) {
@@ -154,4 +285,6 @@ export function getColorFormat(value) {
 	if (isRGBString(value)) return FORMATS.RGB_STRING;
 	if (isRGBArray(value)) return FORMATS.RGB_ARRAY;
 	if (isRGBAArray(value)) return FORMATS.RGBA_ARRAY;
+	if (isVec3String(value)) return FORMATS.VEC3_STRING;
+	if (isVec4String(value)) return FORMATS.VEC4_STRING;
 };
