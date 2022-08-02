d289a19295bfc8fac289fc49b319cafbea96e008
Raphaël Améaume <raphael.ameaume@gmail.com>
2022-08-03 00:05:07 +0200

    Extract css to public to avoid flashing

--- src/client/app/App.svelte
+++ src/client/app/App.svelte
@@ -1,24 +1,8 @@
 <script>
-import { tick } from "svelte";
-import Loading from "./ui/Loading.svelte";
 import Layout from "./ui/Layout.svelte";
 import TriggersSetup from "./ui/TriggersSetup.svelte";
 import "./client";
-import Clone from "./ui/Clone.svelte";
-
-async function start() {
-	await tick();
-}
-
 </script>
 
-{#await start() }
-	<Loading />
-{:then}
-	<TriggersSetup />
-	{#if window.location.search.includes("clone")}
-		<Clone />
-	{:else}
-	<Layout />
-	{/if}
-{/await}
+<TriggersSetup />
+<Layout />
