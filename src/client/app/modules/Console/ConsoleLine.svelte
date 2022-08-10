33fda55c2c314e2f77f3bb4a696f1ae014a76cae
Raphaël Améaume <raphael.ameaume@gmail.com>
2022-08-10 20:09:05 +0200

    Console: improve display and handle add/remove module from layout

--- src/client/app/modules/Console/ConsoleLine.svelte
+++ src/client/app/modules/Console/ConsoleLine.svelte
@@ -43,15 +43,17 @@
 	{:else if log.level === 'table'}
 		<JSONTree value={log.args[0]} />
 	{:else}
-		{#each log.args as arg}
-			{#if typeof arg === "string"}
-				<span class="string">{typeof arg} {arg}</span>
-			{:else if typeof arg === "function"}
-				<div class="function">{arg.toString()}</div>
-			{:else}
-				<JSONTree value={arg} />
-			{/if}
-		{/each}
+		<div class="args">
+			{#each log.args as arg}
+				{#if typeof arg === "string"}
+					<span class="string">{arg}</span>
+				{:else if typeof arg === "function"}
+					<div class="function">{arg.toString()}</div>
+				{:else}
+					<JSONTree value={arg} />
+				{/if}
+			{/each}
+		</div>
 	{/if}
 	{#each new Array(level - 1) as _, idx}
 		<div class="outline" style="left: {idx * 15 + 15}px" />
@@ -73,167 +75,177 @@
 {/if}
 
 <style>
-	.log {
-		--json-tree-string-color: rgba(255, 255, 255, 0.5);
-		--arrow-sign: rgba(255, 255, 255, 0.5);
-		--json-tree-font-size: var(--font-size-input);
-		--json-tree-font-family: var(--font-mono);
-		--json-tree-number-color: var(--color-active);
-		--json-tree-symbol-color: #cb3f41;
-		--json-tree-boolean-color: var(--color-active);
-		--json-tree-function-color: var(--color-active);
-		/* --json-tree-number-color: #3029cf; */
-		--json-tree-property-color: var(--color-text);
-		--json-tree-label-color: var(--color-active);
-		--json-tree-arrow-color: #727272;
-		--json-tree-null-color: #8d8d8d;
-		--json-tree-undefined-color: #8d8d8d;
-		--json-tree-date-color: #8d8d8d;
-		--json-tree-li-indentation: 12px;
+.log {
+	--json-tree-string-color: rgba(255, 255, 255, 0.5);
+	--arrow-sign: rgba(255, 255, 255, 0.5);
+	--json-tree-font-size: var(--font-size-input);
+	--json-tree-font-family: var(--font-mono);
+	--json-tree-number-color: var(--color-active);
+	--json-tree-symbol-color: #cb3f41;
+	--json-tree-boolean-color: var(--color-active);
+	--json-tree-function-color: var(--color-active);
+	/* --json-tree-number-color: #3029cf; */
+	--json-tree-property-color: var(--color-text);
+	--json-tree-label-color: var(--color-active);
+	--json-tree-arrow-color: #727272;
+	--json-tree-null-color: #8d8d8d;
+	--json-tree-undefined-color: #8d8d8d;
+	--json-tree-date-color: #8d8d8d;
+	--json-tree-li-indentation: 12px;
 
-		border-bottom: 1px solid #3A3A3A;
-		border-top: 1px solid #3A3A3A;
-		padding: 4px 0 3px;
-		display: flex;
-		position: relative;
+	position: relative;
+	
+	display: flex;
+	padding: 4px 0 3px;
 
-		color: rgba(255, 255, 255, 0.5);
-		/* font-weight: 700; */
-	}
+	color: rgba(255, 255, 255, 0.5);
 
-	.string {
-		display: block;
-		font-size: var(--font-size-input);
-	}
+	border-bottom: 1px solid #3A3A3A;
+	border-top: 1px solid #3A3A3A;
+}
 
-	.function {
-		font-size: var(--font-size-input);
-		font-style: italic;
-		white-space: pre;
-		tab-size: 2em;
-	}
+.args {
+	display: flex;
+	flex-direction: column;
+}
 
-	.log:first-child:not(.console-error) {
-		border-top: none;
-	}
+.string {
+	display: block;
+	font-size: var(--font-size-input);
+}
 
-	.log:last-child:not(.console-error) {
-		border-bottom: none;
-	}
+.function {
+	font-size: var(--font-size-input);
+	font-style: italic;
+	white-space: pre;
+	tab-size: 2em;
+}
 
-	.log:not(:first-child) {
-		margin-top: -1px;
-	}
+:global(.log ul > .Number) {
+	margin-left: calc(var(--li-identation) * -1);
+}
 
-	/* .log > :global(*) {
-		margin-right: 10px;
-	} */
+.log:first-child:not(.console-error) {
+	border-top: none;
+}
 
-	.console-warn, .console-system-warn {
-		--json-tree-string-color: #F3AB26;
-		background: #332B00;
-		border-color: #665600;
-	}
+.log:last-child:not(.console-error) {
+	border-bottom: none;
+}
 
-	.console-error, .console-assert {
-		--json-tree-string-color: #FF8081;
+.log:not(:first-child) {
+	margin-top: -1px;
+}
 
-		background: #2A0000;
-		border-color: #5C0000;
-	}
+/* .log > :global(*) {
+	margin-right: 10px;
+} */
 
-	.console-group, .arrow {
-		cursor: pointer;
-		user-select: none;
-	}
+.console-warn, .console-system-warn {
+	--json-tree-string-color: #F3AB26;
+	background: #332B00;
+	border-color: #665600;
+}
 
-	.log :global(.arrow) {
-		margin-right: 10px;
-	}
+.console-error, .console-assert {
+	--json-tree-string-color: #FF8081;
 
-	.log :global(.String) {
-		white-space: pre-wrap;
-	}
+	background: #2A0000;
+	border-color: #5C0000;
+}
 
-	.console-trace, .console-assert {
-		border-bottom: none;
-	}
+.console-group, .arrow {
+	cursor: pointer;
+	user-select: none;
+}
 
-	.console-assert + .trace {
-		background: #fff0f0;
-		border-color: #fed6d7;
-	}
+.log :global(.arrow) {
+	margin-right: 10px;
+}
 
-	.trace {
-		border-bottom: 1px solid #eee;
-		font-size: 12px;
-		font-family: var(--font-mono);
-		padding: 4px 0 2px;
-	}
+.log :global(.String) {
+	white-space: pre-wrap;
+}
 
-	.trace > :global(div) {
-		margin-left: 15px;
-	}
+.console-trace, .console-assert {
+	border-bottom: none;
+}
 
-	.count {
-		position: relative;
+.console-assert + .trace {
+	background: #fff0f0;
+	border-color: #fed6d7;
+}
 
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		margin-right: 4px;
-		
-		font-size: 9px;
-		padding: 0 4px;
-	}
+.trace {
+	border-bottom: 1px solid #eee;
+	font-size: 12px;
+	font-family: var(--font-mono);
+	padding: 4px 0 2px;
+}
 
-	.count:before {
-		content: '';
+.trace > :global(div) {
+	margin-left: 15px;
+}
 
-		position: absolute;
+.count {
+	position: relative;
 
-		width: 100%;
-		min-width: 16px;
-		height: 16px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin-right: 4px;
+	
+	font-size: 9px;
+	padding: 0 4px;
+}
 
-		border-radius: 9px;
-		background-color: var(--color-lightblack);
-	}
+.count:before {
+	content: '';
 
-	.count span {
-		position: relative;
-	}
+	position: absolute;
 
-	.outline {
-		border-left: 1px solid #9c9cab;
-		position: absolute;
-		top: 0;
-		bottom: -1px;
-	}
+	width: 100%;
+	min-width: 16px;
+	height: 16px;
 
-	.arrow {
-		position: absolute;
-		font-size: 0.6em;
-		transition: 150ms;
-		transform-origin: 50% 50%;
-		transform: translateY(1px) translateX(-50%);
-	}
+	border-radius: 9px;
+	background-color: var(--color-lightblack);
+}
 
-	.arrow.expand {
-		transform: translateY(1px) translateX(-50%) rotateZ(90deg);
-	}
+.count span {
+	position: relative;
+}
 
-	.title {
-		font-family: var(--font-mono);
-		font-size: 13px;
-		font-weight: bold;
-		padding-left: 11px;
-		height: 19px;
-	}
+.outline {
+	border-left: 1px solid #9c9cab;
+	position: absolute;
+	top: 0;
+	bottom: -1px;
+}
 
-	.assert {
-		padding-left: 11px;
-		font-weight: bold;
-		/* color: #da106e; */
-	}
+.arrow {
+	position: absolute;
+	font-size: 0.6em;
+	transition: 150ms;
+	transform-origin: 50% 50%;
+	transform: translateY(1px) translateX(-50%);
+}
+
+.arrow.expand {
+	transform: translateY(1px) translateX(-50%) rotateZ(90deg);
+}
+
+.title {
+	font-family: var(--font-mono);
+	font-size: 13px;
+	font-weight: bold;
+	padding-left: 11px;
+	height: 19px;
+}
+
+.assert {
+	padding-left: 11px;
+	font-weight: bold;
+	/* color: #da106e; */
+}
 </style>
