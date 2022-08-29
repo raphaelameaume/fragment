4db0fcf73a6a22940f012558e1b0d96ff2200de8
Raphaël Améaume <raphael.ameaume@gmail.com>
2022-08-29 16:04:35 +0200

    Update examples

--- examples/circle-2d.js
+++ examples/circle-2d.js
@@ -4,7 +4,23 @@ export let props = {
     params: {
       min: 50,
       max: 250
+    },
+  },
+  size: {
+    value: 100,
+    params: {
+      min: 50,
+      max: 250
+    },
+  },
+  shape: {
+    value: "circle",
+    params: {
+      options: ["circle", "square"]
     }
+  },
+  fill: {
+    value: "#0000ff",
   }
 };
 
@@ -17,11 +33,18 @@ export let update = ({ context, width, height }) => {
 
   // draw circle
   const radius = props.radius.value;
+  const shape = props.shape.value;
+  const size = props.size.value;
 
-  context.fillStyle = '#0000ff';
-  context.beginPath();
-  context.arc(width * 0.5, height * 0.5, radius, 0, 2 * Math.PI, false);
-  context.fill();
+  context.fillStyle = props.fill.value;
+
+  if (shape === "circle") {
+    context.beginPath();
+    context.arc(width * 0.5, height * 0.5, radius, 0, 2 * Math.PI, false);
+    context.fill();
+  } else if (shape === "square") {
+    context.fillRect(width * 0.5 - size * 0.5, height * 0.5 - size * 0.5, size, size);
+  }
 };
 
 export let rendering = "2d";
