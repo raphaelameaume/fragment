e53fe8e3f74d8a2eb7b549941ee3a9a78da4ec80
Raphaël Améaume <raphael.ameaume@gmail.com>
2022-08-30 18:03:43 +0200

    remove dead code

--- src/client/app/components/Init.svelte
+++ src/client/app/components/Init.svelte
@@ -2,17 +2,8 @@
 import { assignSketchFiles } from "../triggers/shared.js";
 import { names as sketchFiles } from "../stores/sketches.js";
 import "../utils/glslErrors.js";
+import { displayError } from "../stores/errors.js";
 
 assignSketchFiles(sketchFiles);
-
-window.onunhandledrejection = (e) => {
-	console.log("onunhandledrejection");
-	console.log(e);
-};
-
-window.onerror = (e) => {
-	console.log("onError");
-	console.log(e);
-}
 
 </script>
