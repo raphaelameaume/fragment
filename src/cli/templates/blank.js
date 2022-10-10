56dc5d94e4b1f3ec9bde7a87dfa87a453615ac33
Raphaël Améaume <raphael.ameaume@gmail.com>
2022-10-10 10:55:14 +0200

    Add blank template

---
+++ src/cli/templates/blank.js
@@ -0,0 +1,13 @@
+export let props = {};
+
+export let init = ({ canvas, width, height }) => {
+
+};
+
+export let update = ({ width, height, time, deltaTime }) => {
+	
+};
+
+export let resize = ({ width, height, pixelRatio }) => {
+
+};
