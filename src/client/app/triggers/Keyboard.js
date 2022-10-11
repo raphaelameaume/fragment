2667bb8e407436e42b56a8565a10aa11e1e7e717
Raphaël Améaume <raphael.ameaume@gmail.com>
2022-10-11 17:48:06 +0200

    Fix toggling of pause when hitting space

--- src/client/app/triggers/Keyboard.js
+++ src/client/app/triggers/Keyboard.js
@@ -61,7 +61,7 @@ function createTrigger(eventName, collection) {
         const { hot, enabled, ...params } = options;
         const context = getContext();
 
-        const keys = Array.isArray(key) ? key : [...key.split(',').map(k => k.trim())];
+        const keys = Array.isArray(key) ? key : [...key.split(',').map(k => k !== " " ? k.trim() : k)];
         
         const trigger = new Trigger({
             inputType: 'Keyboard',
