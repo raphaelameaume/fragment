b1a1be4bc1c672c93b88e53605be488042b9e5b4
Raphaël Améaume <raphael.ameaume@gmail.com>
2022-11-16 20:53:13 +0100

    SketchFolder: force collapsed state if changed in folder initialization

--- src/client/app/ui/SketchFolder.svelte
+++ src/client/app/ui/SketchFolder.svelte
@@ -4,6 +4,8 @@ import FieldGroup from "./FieldGroup.svelte";
 
 export let folder;
 
+let collapsed0 = folder.collapsed0;
+
 $: collapsed = folder.collapsed;
 $: name = folder.label;
 $: nesting = folder.level;
@@ -22,7 +24,7 @@ onMount(() => {
 
 afterUpdate(() => {
 	// reinit collapsing if value change on init in sketch file
-	if (collapsed !== folder.collapsed0) {
+	if (folder.collapsed0 !== collapsed0) {
 		collapsed = folder.collapsed0;
 	}
 });
