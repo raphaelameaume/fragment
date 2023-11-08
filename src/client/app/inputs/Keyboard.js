16195d7268198a72fb314eed2299393eae06fe4c
Raphaël Améaume <raphael.ameaume@gmail.com>
2023-11-08 17:30:28 +0100

    Prettier formatting

--- src/client/app/inputs/Keyboard.js
+++ src/client/app/inputs/Keyboard.js
@@ -1,21 +1,19 @@
-import Input from "./Input";
+import Input from './Input';
 
 class Keyboard extends Input {
+	/**
+	 *
+	 * @param {KeyboardEvent} event
+	 */
+	getStepFromEvent(event) {
+		if (event.shiftKey) {
+			return 10;
+		} else if (event.altKey) {
+			return 0.1;
+		}
 
-    /**
-     * 
-     * @param {KeyboardEvent} event 
-     */
-    getStepFromEvent(event) {
-        if (event.shiftKey) {
-            return 10;
-        } else if (event.altKey) {
-            return 0.1;
-        }
-
-        return 1;
-    }
-
+		return 1;
+	}
 }
 
 export default new Keyboard();
--- src/client/app/triggers/MIDI.js
+++ src/client/app/triggers/MIDI.js
@@ -1,7 +1,7 @@
-import Trigger from "./Trigger";
-import MIDI from "../inputs/MIDI.js";
-import { addToMapArray, removeFromMapArray } from "../utils";
-import { wildcard, getContext } from "./shared";
+import Trigger from './Trigger';
+import MIDI from '../inputs/MIDI.js';
+import { addToMapArray, removeFromMapArray } from '../utils';
+import { wildcard, getContext } from './shared';
 
 const noteons = new Map();
 const noteoffs = new Map();
@@ -11,33 +11,36 @@ const controlchanges = new Map();
 
 /**
  * Remove listeners from a specific context
- * @param {string} context 
+ * @param {string} context
  */
 export const removeHotListeners = (context) => {
-    function removeHotFrom(collection) {
-        for (let trigger of collection) {
-            const [key, triggers] = trigger;
-            
-            collection.set(key, triggers.filter((trigger) => trigger.context !== context));
-        }
-    }
+	function removeHotFrom(collection) {
+		for (let trigger of collection) {
+			const [key, triggers] = trigger;
 
-    removeHotFrom(noteons);
-    removeHotFrom(noteoffs);
-    removeHotFrom(numberons);
-    removeHotFrom(numberoffs);
-    removeHotFrom(controlchanges);
+			collection.set(
+				key,
+				triggers.filter((trigger) => trigger.context !== context),
+			);
+		}
+	}
+
+	removeHotFrom(noteons);
+	removeHotFrom(noteoffs);
+	removeHotFrom(numberons);
+	removeHotFrom(numberoffs);
+	removeHotFrom(controlchanges);
 };
 
 /**
  * Check all registered listeners for a specific key
- * @param {Map} collection 
- * @param {function} getKey 
+ * @param {Map} collection
+ * @param {function} getKey
  * @returns {function} listener
  */
 function createEventListener(collection, getKey = (event) => event.key) {
-    return (event) => {
-        const key = getKey(event);
+	return (event) => {
+		const key = getKey(event);
 		const { id } = event.srcElement;
 
 		const triggers = [
@@ -46,78 +49,101 @@ function createEventListener(collection, getKey = (event) => event.key) {
 		];
 
 		if (MIDI.enabled && id === MIDI.selectedInputID) {
-			triggers.forEach(trigger => {
+			triggers.forEach((trigger) => {
 				trigger.run(event);
 			});
 		}
-    };
+	};
 }
 
 /**
  * Create a registering function for a specific event
- * @param {string} eventName 
- * @param {Map} collection 
- * @returns {function} createListener 
+ * @param {string} eventName
+ * @param {Map} collection
+ * @returns {function} createListener
  */
 function createTrigger(eventName, collection) {
-    return (key, fn, options = {}) => {
-        if (typeof key === "function") {
-            if (typeof fn === "object") {
-                options = {
-                    ...options,
-                    ...fn,
-                };
-            }
+	return (key, fn, options = {}) => {
+		if (typeof key === 'function') {
+			if (typeof fn === 'object') {
+				options = {
+					...options,
+					...fn,
+				};
+			}
 
-            fn = key;
-            key = "*";
+			fn = key;
+			key = '*';
 
-            if (options.key) {
-                key = options.key;
-            }
-        }
+			if (options.key) {
+				key = options.key;
+			}
+		}
 
-        const { hot, enabled, ...params } = options;
-        let keys = Array.isArray(key) ? key : [...key.split(',').map(k => k.trim())];
+		const { hot, enabled, ...params } = options;
+		let keys = Array.isArray(key)
+			? key
+			: [...key.split(',').map((k) => k.trim())];
 
-        if (["onControlChange", "onNumberOn", "onNumberOff"].includes(eventName)) {
-            keys = keys.map((k) => Number(k));
-        }
+		if (
+			['onControlChange', 'onNumberOn', 'onNumberOff'].includes(eventName)
+		) {
+			keys = keys.map((k) => Number(k));
+		}
 
-        if (!MIDI.enabled) {
-            MIDI.request();
-        }
+		if (!MIDI.enabled) {
+			MIDI.request();
+		}
 
-        const context = getContext();
+		const context = getContext();
 
-        const trigger = new Trigger({
-            inputType: 'MIDI',
-            eventName,
-            fn,
-            params: {...params, key: keys },
-            hot,
-            context,
-            enabled,
-            destroy : () => {
-                keys.forEach((key) => {
-                    removeFromMapArray(collection, key, (item) => item.id === trigger.id);
-                });
-            }
-        });
+		const trigger = new Trigger({
+			inputType: 'MIDI',
+			eventName,
+			fn,
+			params: { ...params, key: keys },
+			hot,
+			context,
+			enabled,
+			destroy: () => {
+				keys.forEach((key) => {
+					removeFromMapArray(
+						collection,
+						key,
+						(item) => item.id === trigger.id,
+					);
+				});
+			},
+		});
 
-        keys.forEach(k => {
-            addToMapArray(collection, k, trigger);
-        });
+		keys.forEach((k) => {
+			addToMapArray(collection, k, trigger);
+		});
 
-        return trigger;
-    };
-};
+		return trigger;
+	};
+}
 
-MIDI.addEventListener("noteon", createEventListener(noteons, (event) => event.note.name));
-MIDI.addEventListener("noteoff", createEventListener(noteoffs, (event) => event.note.name));
-MIDI.addEventListener("noteon", createEventListener(numberons, (event) => event.note.number));
-MIDI.addEventListener("noteoff", createEventListener(numberoffs, (event) => event.note.number));
-MIDI.addEventListener("controlchange", createEventListener(controlchanges, (event) => event.note.number));
+MIDI.addEventListener(
+	'noteon',
+	createEventListener(noteons, (event) => event.note.name),
+);
+MIDI.addEventListener(
+	'noteoff',
+	createEventListener(noteoffs, (event) => event.note.name),
+);
+MIDI.addEventListener(
+	'noteon',
+	createEventListener(numberons, (event) => event.note.number),
+);
+MIDI.addEventListener(
+	'noteoff',
+	createEventListener(numberoffs, (event) => event.note.number),
+);
+MIDI.addEventListener(
+	'controlchange',
+	createEventListener(controlchanges, (event) => event.note.number),
+);
 
 export const onNoteOn = createTrigger('onNoteOn', noteons);
 export const onNoteOff = createTrigger('onNoteOff', noteoffs);
