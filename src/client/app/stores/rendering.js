6563db37a457e6228117f7864a3262c0aa3a818f
Raphaël Améaume <raphael.ameaume@gmail.com>
2023-11-25 11:56:23 +0100

    Prettier formatting

--- src/client/app/stores/rendering.js
+++ src/client/app/stores/rendering.js
@@ -1,89 +1,104 @@
-import { writable } from "svelte/store";
-import { client } from "../client";
-import { createStore } from "./utils";
-import { getDimensionsForPreset } from "../lib/presets";
+import { writable } from 'svelte/store';
+import { client } from '../client';
+import { createStore } from './utils';
+import { getDimensionsForPreset } from '../lib/presets';
 
 export const SIZES = {
-    FIXED: "fixed",
-    PRESET: "preset",
-    ASPECT_RATIO: "aspect-ratio",
-    WINDOW: "window",
-    SCALE: "scale",
+	FIXED: 'fixed',
+	PRESET: 'preset',
+	ASPECT_RATIO: 'aspect-ratio',
+	WINDOW: 'window',
+	SCALE: 'scale',
 };
 
-export const rendering = createStore(`rendering`, {
-    width: 1024,
-    height: 1024,
-    pixelRatio: 1,
-    resizing: SIZES.FIXED,
-    aspectRatio: 1,
-    scale: 1,
-    preset: 'a4',
-}, {
-    persist: !__BUILD__,
-    reset: false, 
-});
+export const rendering = createStore(
+	`rendering`,
+	{
+		width: 1024,
+		height: 1024,
+		pixelRatio: 1,
+		resizing: SIZES.FIXED,
+		aspectRatio: 1,
+		scale: 1,
+		preset: 'a4',
+	},
+	{
+		persist: !__BUILD__,
+		reset: false,
+	},
+);
 
 export const monitors = createStore('monitors', []);
 export const preview = createStore('preview', null);
 
 export const override = (config) => {
-    const { canvasSize = SIZES.WINDOW } = config;
-    const resizing = canvasSize;
+	const { canvasSize = SIZES.WINDOW } = config;
+	const resizing = canvasSize;
 
-    const overrides = {
-        resizing,
-    };
+	const overrides = {
+		resizing,
+	};
 
-    if (config.dimensions && config.dimensions.length === 2) {
-        const { dimensions } = config;
-        overrides.width = dimensions[0];
-        overrides.height = dimensions[1];
+	if (config.dimensions && config.dimensions.length === 2) {
+		const { dimensions } = config;
+		overrides.width = dimensions[0];
+		overrides.height = dimensions[1];
 
-        if (!config.canvasSize) {
-            overrides.resizing = SIZES.FIXED;
-        }
-    }
+		if (!config.canvasSize) {
+			overrides.resizing = SIZES.FIXED;
+		}
+	}
 
-    if (resizing === SIZES.PRESET) {
-        if (config.preset) {
-            const [ width, height ] = getDimensionsForPreset(config.preset, { pixelsPerInch: 300 });
+	if (resizing === SIZES.PRESET) {
+		if (config.preset) {
+			const [width, height] = getDimensionsForPreset(config.preset, {
+				pixelsPerInch: 300,
+			});
 
-            overrides.width = width;
-            overrides.height = height;
-        } else {
-            overrides.resizing = SIZES.WINDOW;
-            console.warn(`Cannot apply canvasSize preset if 'preset' is not specified in config.`);
-        }
-    }
+			overrides.width = width;
+			overrides.height = height;
+		} else {
+			overrides.resizing = SIZES.WINDOW;
+			console.warn(
+				`Cannot apply canvasSize preset if 'preset' is not specified in config.`,
+			);
+		}
+	}
 
-    if (resizing === SIZES.ASPECT_RATIO) {
-        if (isNaN(config.aspectRatio)) {
-            overrides.resizing = SIZES.WINDOW;
-            console.warn(`Cannot apply canvasSize:"aspectRatio" if 'aspectRatio' is not specified in config.`);
-        }
-    }
+	if (resizing === SIZES.ASPECT_RATIO) {
+		if (isNaN(config.aspectRatio)) {
+			overrides.resizing = SIZES.WINDOW;
+			console.warn(
+				`Cannot apply canvasSize:"aspectRatio" if 'aspectRatio' is not specified in config.`,
+			);
+		}
+	}
 
-    if (resizing === SIZES.SCALE) {
-        if (!config.dimensions) {
-            console.warn(`Cannot apply canvasSize:"scale" if no dimensions are specified.`);
-            overrides.resizing = SIZES.WINDOW;
-        }
+	if (resizing === SIZES.SCALE) {
+		if (!config.dimensions) {
+			console.warn(
+				`Cannot apply canvasSize:"scale" if no dimensions are specified.`,
+			);
+			overrides.resizing = SIZES.WINDOW;
+		}
 
-        if (isNaN(config.scale)) {
-            console.warn(`Cannot apply canvasSize:"scale" if 'scale' is not specified in config.`);
-            overrides.resizing = SIZES.WINDOW;
-        } else {
-            overrides.scale = config.scale;
-        }
-    }
+		if (isNaN(config.scale)) {
+			console.warn(
+				`Cannot apply canvasSize:"scale" if 'scale' is not specified in config.`,
+			);
+			overrides.resizing = SIZES.WINDOW;
+		} else {
+			overrides.scale = config.scale;
+		}
+	}
 
-    if (config.pixelRatio) {
-        const { pixelRatio } = config;
-        overrides.pixelRatio = typeof pixelRatio === "function" ? pixelRatio() : pixelRatio;
-    }
+	if (config.pixelRatio) {
+		const { pixelRatio } = config;
+		overrides.pixelRatio =
+			typeof pixelRatio === 'function' ? pixelRatio() : pixelRatio;
+	}
 
-    rendering.update((curr) => ({...curr, ...overrides}));
+	rendering.update((curr) => ({ ...curr, ...overrides }));
 };
 
 /* sync across clients */
@@ -92,18 +107,20 @@ let isSynchronized = false;
 export const sync = writable(isSynchronized);
 
 function checkForSync({ clientCount } = {}) {
-    let prev = isSynchronized;
-    isSynchronized = clientCount > 0;
+	let prev = isSynchronized;
+	isSynchronized = clientCount > 0;
 
-    if (prev && !isSynchronized) {
-        console.warn("[fragment] Sketch is running at specified framerate.");
-    } else if (!prev && isSynchronized) {
-        console.warn("[fragment] Multiple instances of Fragment detected. Running sketch(s) at simulated framerate.");
-    }
+	if (prev && !isSynchronized) {
+		console.warn('[fragment] Sketch is running at specified framerate.');
+	} else if (!prev && isSynchronized) {
+		console.warn(
+			'[fragment] Multiple instances of Fragment detected. Running sketch(s) at simulated framerate.',
+		);
+	}
 
-    if (prev !== isSynchronized) {
-        sync.set(isSynchronized);
-    }
+	if (prev !== isSynchronized) {
+		sync.set(isSynchronized);
+	}
 }
 
 client.on('start', checkForSync);
