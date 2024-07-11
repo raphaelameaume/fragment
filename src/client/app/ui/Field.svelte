569e3aeb89db532af7b63769b695a8d9e8c3e451
Raphaël Améaume <hello@raphaelameaume.com>
2024-07-11 21:25:46 +0200

    Field: extract icons to components

--- src/client/app/ui/Field.svelte
+++ src/client/app/ui/Field.svelte
@@ -33,10 +33,12 @@
 	import { map } from '../utils/math.utils';
 	import frameDebounce from '../lib/helpers/frameDebounce.js';
 	import { inferFieldType } from '../utils/fields.utils.js';
+	import IconTriggers from '../components/IconTriggers.svelte';
+	import IconLocked from '../components/IconLocked.svelte';
 
 	let {
 		key, 
-		value = $bindable(null), 
+		value = null, 
 		initialValue = value, 
 		context = null,
 		params = $bindable({}),
@@ -139,112 +141,19 @@
 			<div class="field__actions">
 				{#if triggerable && !disabled}
 					<button
-						on:click={toggleTriggers}
+						onclick={toggleTriggers}
 						class="field__action field__action--triggers"
 						class:active={triggersActive}
 					>
-						<svg width="16" height="16" fill="none" viewBox="0 0 24 24">
-							<path
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="1.5"
-								d="M4.75 8H7.25"
-							/>
-							<path
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="1.5"
-								d="M12.75 8H19.25"
-							/>
-							<path
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="1.5"
-								d="M4.75 16H12.25"
-							/>
-							<path
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="1.5"
-								d="M17.75 16H19.25"
-							/>
-							<circle
-								cx="10"
-								cy="8"
-								r="2.25"
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="1.5"
-							/>
-							<circle
-								cx="15"
-								cy="16"
-								r="2.25"
-								stroke="currentColor"
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								stroke-width="1.5"
-							/>
-						</svg>
+						<IconTriggers />
 					</button>
 				{/if}
-				{#if fieldType === 'vec' && !disabled}
+				{#if fieldType === fieldTypes.VEC && !disabled}
 					<button
 						class="field__action field__action--lock"
-						on:click={() => (params.locked = !params.locked)}
+						onclick={() => (params.locked = !params.locked)}
 					>
-						{#if params.locked}
-							<svg
-								class="action__icon"
-								width="16"
-								height="16"
-								fill="none"
-								viewBox="0 0 24 24"
-							>
-								<path
-									stroke="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="1.5"
-									d="M5.75 11.75C5.75 11.1977 6.19772 10.75 6.75 10.75H17.25C17.8023 10.75 18.25 11.1977 18.25 11.75V17.25C18.25 18.3546 17.3546 19.25 16.25 19.25H7.75C6.64543 19.25 5.75 18.3546 5.75 17.25V11.75Z"
-								/>
-								<path
-									stroke="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="1.5"
-									d="M7.75 10.5V10.3427C7.75 8.78147 7.65607 7.04125 8.74646 5.9239C9.36829 5.2867 10.3745 4.75 12 4.75C13.6255 4.75 14.6317 5.2867 15.2535 5.9239C16.3439 7.04125 16.25 8.78147 16.25 10.3427V10.5"
-								/>
-							</svg>
-						{:else}
-							<svg
-								class="action__icon"
-								width="16"
-								height="16"
-								fill="none"
-								viewBox="0 0 24 24"
-							>
-								<path
-									stroke="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="1.5"
-									d="M5.75 11.75C5.75 11.1977 6.19772 10.75 6.75 10.75H17.25C17.8023 10.75 18.25 11.1977 18.25 11.75V17.25C18.25 18.3546 17.3546 19.25 16.25 19.25H7.75C6.64543 19.25 5.75 18.3546 5.75 17.25V11.75Z"
-								/>
-								<path
-									stroke="currentColor"
-									stroke-linecap="round"
-									stroke-linejoin="round"
-									stroke-width="1.5"
-									d="M7.75 10.5V9.84343C7.75 8.61493 7.70093 7.29883 8.42416 6.30578C8.99862 5.51699 10.0568 4.75 12 4.75C14 4.75 15.25 6.25 15.25 6.25"
-								/>
-							</svg>
-						{/if}
+						<IconLocked locked={params.locked}/>
 					</button>
 				{/if}
 			</div>
