2248ac755235137f9693c2c1e68d9e7ff0869f91
Raphaël Améaume <hello@raphaelameaume.com>
2025-01-03 17:20:13 +0100

    Expose screenshot in helpers

--- src/client/app/hooks.js
+++ src/client/app/hooks.js
@@ -1,3 +1,4 @@
+import { rendering } from './state/rendering.svelte';
 import { sketchesManager } from './state/sketches.svelte';
 
 import { getContext } from './triggers/shared';
@@ -24,3 +25,28 @@ export let onBeforeRecord = (listener, context = getContext()) => {
 export let onAfterRecord = (listener, context = getContext()) => {
 	sketchesManager.sketches[context]?.onAfterRecord(listener);
 };
+
+/**
+ * Screenshot the sketch it is called from
+ * @param {object} options
+ * @param {string} [options.filename]
+ * @param {function} [options.pattern]
+ * @param {exportDir} [options.pattern ]
+ */
+export async function screenshot({ filename, pattern, exportDir } = {}) {
+	const context = getContext();
+
+	const renders = rendering.renders.filter(
+		(render) => render.sketch.key === context,
+	);
+
+	if (renders.length > 0) {
+		for (let i = 0; i < renders.length; i++) {
+			await renders[i].screenshot({
+				filename,
+				pattern,
+				exportDir,
+			});
+		}
+	}
+}
