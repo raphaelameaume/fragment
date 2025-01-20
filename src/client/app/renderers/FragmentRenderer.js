cc2835d49a80be342f8fe98f67bc6f09904e3839
Raphaël Améaume <hello@raphaelameaume.com>
2025-01-20 15:24:40 +0100

    Fix defines and imports (#134)

--- src/client/app/renderers/FragmentRenderer.js
+++ src/client/app/renderers/FragmentRenderer.js
@@ -1,7 +1,7 @@
 import { fragment } from '../lib/gl';
 import { client } from '@fragment/client';
 import { getShaderPath } from '../utils/glsl.utils';
-import { clearError } from '../state/errors';
+import { clearError } from '../state/errors.svelte';
 
 let frags = [];
 
