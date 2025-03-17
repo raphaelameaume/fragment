e98db7dfe16c0989e2506762bddcbd3535b39dbe
Raphaël Améaume <hello@raphaelameaume.com>
2025-03-17 19:37:15 +0100

    Remove update of rendering

--- src/client/app/state/rendering.svelte.js
+++ src/client/app/state/rendering.svelte.js
@@ -627,8 +627,6 @@ export class Render {
 			onComplete: (params) => {
 				sketch.afterRecord.forEach((fn) => fn(params));
 				this.record = null;
-				this.recording = false;
-				this.paused = false;
 			},
 		});
 	}
