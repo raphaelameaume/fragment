f5e0b2f696bd82faa8a5e6641ad9ef906048900d
Arnaud Rocca <arnaud@toosoon.paris>
2025-03-27 11:53:08 +0100

    feature: customizable sketch params types

--- src/types/sketch.d.ts
+++ src/types/sketch.d.ts
@@ -2,21 +2,21 @@ import type { Props } from './props';
 import type { Rendering, MountParams, RendererParams } from './renderers';
 import type { Frag } from './gl';
 
-export type Load<R extends Rendering> = (
+export type Load<R extends Rendering | object> = (
 	params: RendererParams &
 		MountParams<R> & {
 			publicPath: string;
 		},
 ) => Promise<void> | void;
 
-export type Init<R extends Rendering> = (
+export type Init<R extends Rendering | object> = (
 	params: RendererParams &
 		MountParams<R> & {
 			publicPath: string;
 		},
 ) => Promise<void> | void;
 
-export type Update<R extends Rendering> = (
+export type Update<R extends Rendering | object> = (
 	params: RendererParams &
 		MountParams<R> & {
 			time: number;
@@ -26,11 +26,11 @@ export type Update<R extends Rendering> = (
 		},
 ) => void;
 
-export type Resize<R extends Rendering> = (
+export type Resize<R extends Rendering | object> = (
 	params: RendererParams & MountParams<R>,
 ) => void;
 
-export type Dispose<R extends Rendering> = () => void;
+export type Dispose<R extends Rendering | object> = () => void;
 
 export type FilenamePattern = (params: {
 	filename: string;
