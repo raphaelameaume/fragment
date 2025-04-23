5854acad3b64eb76f908756cd151907b590acd07
Raphaël Améaume <hello@raphaelameaume.com>
2025-04-23 10:27:13 +0200

    feat: change inputs behaviour

--- src/client/app/inputs/Input.js
+++ src/client/app/inputs/Input.js
@@ -1,6 +1,36 @@
 class Input {
-	constructor() {
+	constructor({ type } = {}) {
+		this.type = type;
 		this.enabled = true;
+	}
+
+	createTrigger(
+		fn,
+		{
+			eventName,
+			collection,
+			context = getContext(),
+			hot,
+			enabled,
+			...params
+		},
+	) {
+		const trigger = new Trigger({
+			inputType: this.type,
+			eventName,
+			context,
+			hot,
+			fn,
+			enabled,
+			params,
+			destroy: () => {
+				const index = collection.findIndex((t) => t === trigger);
+
+				collection.splice(index, 1);
+			},
+		});
+
+		return trigger;
 	}
 
 	enable() {
--- src/client/app/inputs/Mouse.js
+++ src/client/app/inputs/Mouse.js
@@ -1,5 +1,55 @@
 import Input from './Input';
 
-class Mouse extends Input {}
+class Mouse extends Input {
+	constructor() {
+		super({
+			type: 'Mouse',
+		});
+
+		this.triggersDown = [];
+		this.triggersUp = [];
+		this.triggersClick = [];
+		this.triggersMove = [];
+	}
+
+	getTriggers(eventName) {
+		if (eventName === 'onMouseDown') {
+			return this.triggersDown;
+		} else if (eventName === 'onMouseUp') {
+			return this.triggersUp;
+		} else if (eventName === 'onMouseMove') {
+			return this.triggersMove;
+		} else if (eventName === 'onClick') {
+			return this.triggersClick;
+		}
+	}
+
+	runTriggers(event, context, collection) {
+		if (!this.enabled) return;
+
+		const triggers = collection.filter(
+			(trigger) =>
+				trigger.context === context || trigger.context === wildcard,
+		);
+
+		triggers.forEach((trigger) => {
+			trigger.run(event);
+		});
+	}
+
+	onMouseDown(event, context) {
+		this.runTriggers(event, context, this.triggersDown);
+	}
+
+	onMouseUp(event, context) {
+		this.runTriggers(event, context, this.triggersUp);
+	}
+
+	onMouseMove(event, context) {
+		this.runTriggers(event, context, this.triggersMove);
+	}
+
+	onClick() {}
+}
 
 export default new Mouse();
