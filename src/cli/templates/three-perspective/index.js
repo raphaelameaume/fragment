2aa9d81d67253820329b12100495cb7c2255fca8
Raphaël Améaume <hello@raphaelameaume.com>
2026-01-16 18:19:59 +0100

    feat: add resolution to 2d and three templates

--- src/cli/templates/three-perspective/index.js
+++ src/cli/templates/three-perspective/index.js
@@ -4,6 +4,8 @@ import * as THREE from 'three';
 let scene;
 /** @type {THREE.OrthographicCamera} */
 let camera;
+/** @type {THREE.Vector2} */
+let resolution = new THREE.Vector2();
 
 /**
  * @param {object} params
@@ -50,7 +52,10 @@ export const update = ({ renderer, time, deltaTime }) => {
  * @param {number} params.height
  * @param {number} params.pixelRatio
  */
-export const resize = ({ width, height }) => {
+export const resize = ({ width, height, pixelRatio }) => {
+	resolution.x = width * pixelRatio;
+	resolution.y = height * pixelRatio;
+
 	camera.aspect = width / height;
 	camera.updateProjectionMatrix();
 };
